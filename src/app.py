d2a120245496cdef9a0d293d95c885e5eba802c7
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-18 17:41:29 +0000

    set jinja2 in app.py

--- src/app.py
+++ src/app.py
@@ -1,17 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import Response
+from fastapi.templating import Jinja2Templates
+
+from pathlib import Path
 
 
 
 app: FastAPI = FastAPI()
 
+BASE_DIR = Path(__file__).resolve().parent
+templates  = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 # endpoints are defined here
 
 @app.get('/')
-def index():
-    return {
-        "message" : "Hello, World!"
+def index(request: Request) -> Response:
+    context: dict[str, str] = {
+        "name" : "Nelmatrix"
     }
+    return templates.TemplateResponse(request, "index.html", context)
 
 
 # EOSC
