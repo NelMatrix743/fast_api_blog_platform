e30501c8372ac1572048f65bbc62c36db7aedee3
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-19 18:22:01 +0000

    improve app.py

--- src/app.py
+++ src/app.py
@@ -1,15 +1,22 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import Response
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from pathlib import Path
 
 
 
+SRC_DIR = Path(__file__).resolve().parent
+
+TEMPLATE_DIR: str = SRC_DIR / "templates"
+STATIC_DIR: str = SRC_DIR / "static"
+
+templates  = Jinja2Templates(directory=TEMPLATE_DIR)
+
 app: FastAPI = FastAPI()
 
-SRC_DIR = Path(__file__).resolve().parent
-templates  = Jinja2Templates(directory=str(SRC_DIR / "templates"))
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
 # endpoints are defined here
 
