940962b698adff52dc557e4fd5bcf3ea42170219
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-05-06 03:08:42 +0000

    refactor: remove explicit author field from post create schema

--- src/schemas.py
+++ src/schemas.py
@@ -21,7 +21,6 @@ class UserResponse(UserBase):
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
-    author: str = Field(min_length=1, max_length=50)
 
 
 class PostCreate(PostBase):
