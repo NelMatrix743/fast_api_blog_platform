212bb8b172ef9eb776f18b5e542de44a8760e726
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-05-06 03:07:47 +0000

    feat: define base structure for user validation schemas

--- src/schemas.py
+++ src/schemas.py
@@ -1,4 +1,21 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=20)
+    email: EmailStr = Field(max_length=120)
+
+
+class UserCreate(UserBase):
+    pass
+
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    image_file: str | None
+    image_path: str
 
 
 class PostBase(BaseModel):
